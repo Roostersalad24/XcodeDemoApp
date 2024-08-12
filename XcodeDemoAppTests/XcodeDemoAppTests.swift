bb4a1b34eef127a83afbc1c51837b3f0afbdd1d7
Roostersalad24 <AndrewJohnsonmga@gmail.com>
2024-08-12 15:46:24 -0500

    Initial Commit

---
+++ XcodeDemoAppTests/XcodeDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppTests.swift
+//  XcodeDemoAppTests
+//
+//  Created by Andrew Johnson on 8/12/24.
+//
+
+import Testing
+@testable import XcodeDemoApp
+
+struct XcodeDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
